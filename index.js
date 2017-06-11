a8562e5ebea5ae0f1e08f0fde9190a3cdd278009
Jose Miguel Montes <josemontesp@gmail.com>
2017-06-11 19:20:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+const request = require('request-promise');
+const config = require('./config.js');
+
+const apiUrlBtc = 'https://www.surbtc.com/api/v2/markets/btc-clp/ticker';
+const apiUrlEth = 'https://www.cryptomkt.com/api/ethclp/240.json';
+
+const btcAmount = config.btcAmount;
+const CLPInvestedInBTC = config.CLPInvestedInBTC;
+
+const ethAmount = config.ethAmount;
+const CLPInvestedInETH = config.CLPInvestedInETH;
+
+let clpbtcEarnings;
+let clpEthEarnings;
+
+request(apiUrlBtc)
+.then(r => {
+	let btcValue = (Number)(JSON.parse(r).ticker.max_bid[0]);
+	clpbtcEarnings = btcValue*btcAmount - CLPInvestedInBTC;
+	console.log(`Inversión BTC:
+	 CLP invertido: ${CLPInvestedInBTC}
+	 ganancias: ${clpbtcEarnings}
+	 Porcentaje total de cambio: ${(clpbtcEarnings/CLPInvestedInBTC*100).toFixed(2)}%\n`);
+	return request(apiUrlEth);
+})
+.then(r => {
+	let ethValue = (Number)(JSON.parse(r).data.prices_ask.values[0].close_price);
+	clpEthEarnings = Math.round(ethValue * ethAmount - CLPInvestedInETH);
+	console.log(`Inversión ETH:
+	 CLP invertido: ${CLPInvestedInETH}
+	 ganancias: ${clpEthEarnings}
+	 Porcentaje total de cambio: ${(clpEthEarnings/CLPInvestedInETH*100).toFixed(2)}%\n`);
+
+	console.log(`Inversión total:
+	 CLP invertido: ${CLPInvestedInBTC + CLPInvestedInETH}
+	 ganancias: ${clpbtcEarnings + clpEthEarnings}
+	 Porcentaje total de cambio: ${((clpbtcEarnings + clpEthEarnings)/(CLPInvestedInBTC + CLPInvestedInETH)*100).toFixed(2)}%\n`);
+})
+.catch(console.error);
+
