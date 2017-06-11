151e07273ad64c6fbb24614c219f565d34e61f83
Jose Miguel Montes <josemontesp@gmail.com>
2017-06-11 18:22:39 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+'use strict';
+module.exports = {
+	btcAmount: 0,// Cantidad de Bitcoins comprados
+	CLPInvestedInBTC: 0,// Cantidad de CLP invertidos en Bitcoin
+	ethAmount: 0,// Cantidad de Ethereum comprados
+	CLPInvestedInETH: 0// Cantidad de CLP invertidos en Ethereum
+};
