456de4e334021926300524f677ed3af7b83d5531
Jose Miguel Montes <josemontesp@gmail.com>
2017-06-15 20:49:01 -0300

    Precio actual de la moneda y separadores de miles

--- index.js
+++ index.js
@@ -19,27 +19,42 @@ const CLPInvestedInETH = config.CLPInvestedInETH;
 let clpbtcEarnings;
 let clpEthEarnings;
 
+function separators() {
+	let x = this.toString();
+  var pattern = /(-?\d+)(\d{3})/;
+  while (pattern.test(x))
+      x = x.replace(pattern, "$1.$2");
+  return x;
+};
+
+Number.prototype.s = separators;
+String.prototype.s = separators;
+
 Promise.all([request(apiUrlBtc),request(apiUrlEth)]) 
 .then(r => {
 	spinner.stop(true);
 	let btcValue = (Number)(JSON.parse(r[0]).ticker.max_bid[0]);
 	clpbtcEarnings = btcValue*btcAmount - CLPInvestedInBTC;
 	console.log(`Inversión BTC:
-	 CLP invertido: ${CLPInvestedInBTC}
-	 ganancias: ${clpbtcEarnings}
-	 Porcentaje total de cambio: ${(clpbtcEarnings/CLPInvestedInBTC*100).toFixed(2)}%\n`);
+	 CLP invertido: ${CLPInvestedInBTC.s()}
+	 ganancias: ${clpbtcEarnings.s()}
+	 Porcentaje total de cambio: ${(clpbtcEarnings/CLPInvestedInBTC*100).toFixed(2).s()}%
+	 Precio BTC-CLP: ${btcValue.s()}
+	 `);
 	
 	let ethValue = (Number)(JSON.parse(r[1]).data.prices_ask.values[0].close_price);
 	clpEthEarnings = Math.round(ethValue * ethAmount - CLPInvestedInETH);
 	console.log(`Inversión ETH:
-	 CLP invertido: ${CLPInvestedInETH}
-	 ganancias: ${clpEthEarnings}
-	 Porcentaje total de cambio: ${(clpEthEarnings/CLPInvestedInETH*100).toFixed(2)}%\n`);
+	 CLP invertido: ${CLPInvestedInETH.s()}
+	 ganancias: ${clpEthEarnings.s()}
+	 Porcentaje total de cambio: ${(clpEthEarnings/CLPInvestedInETH*100).toFixed(2).s()}%
+	 Precio ETH-CLP: ${ethValue.s()}
+	 `);
 
 	console.log(`Inversión total:
-	 CLP invertido: ${CLPInvestedInBTC + CLPInvestedInETH}
-	 ganancias: ${clpbtcEarnings + clpEthEarnings}
-	 Porcentaje total de cambio: ${((clpbtcEarnings + clpEthEarnings)/(CLPInvestedInBTC + CLPInvestedInETH)*100).toFixed(2)}%\n`);
+	 CLP invertido: ${(CLPInvestedInBTC + CLPInvestedInETH).s()}
+	 ganancias: ${(clpbtcEarnings + clpEthEarnings).s()}
+	 Porcentaje total de cambio: ${((clpbtcEarnings + clpEthEarnings)/(CLPInvestedInBTC + CLPInvestedInETH)*100).toFixed(2).s()}%\n`);
 })
 .catch(console.error);
 
